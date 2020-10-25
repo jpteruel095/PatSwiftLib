d483ff640711762a2b876266e2f66e4a98f4d19f
John Patrick <jpteruel95@gmail.com>
2020-10-25 19:52:15 +0800

    Updated functions

---
+++ Sources/PatWeb/PRequest+RequestHandling.swift
@@ -0,0 +1,215 @@
+//
+//  PRequest+RequestHandling.swift
+//  PatSwiftLib
+//
+//  Created by John Patrick Teruel on 10/25/20.
+//
+
+import Alamofire
+import SwiftyJSON
+import ObjectMapper
+
+public extension PRequest{
+    func request(){
+        requestArrayWithProgress()
+    }
+    
+    func requestSingle(completion: @escaping RequestCompletionSingleClosure){
+        requestArrayWithProgress(completion: { models, error in
+            if let error = error{
+                completion(nil, error)
+                return
+            }
+            
+            guard let model = models.first else{
+                completion(nil, Helpers.makeError(with: "Could not retrieve \(ResultingModel.self) record"))
+                return
+            }
+            
+            completion(model, nil)
+        })
+    }
+    
+    func requestMultiple(completion: @escaping RequestCompletionMultipleClosure){
+        self.requestArrayWithProgress(completion: completion)
+    }
+    
+    func requestSingleWithProgress(_ progressCallback:@escaping ((Progress) -> Void),
+                                   completion: @escaping RequestCompletionSingleClosure){
+        requestArrayWithProgress(progressCallback, completion: { models, error in
+            if let error = error{
+                completion(nil, error)
+                return
+            }
+            
+            guard let model = models.first else{
+                completion(nil, Helpers.makeError(with: "Could not retrieve \(ResultingModel.self) record"))
+                return
+            }
+            
+            completion(model, nil)
+        })
+    }
+    
+    func requestMultipleWithProgress(_ progressCallback:@escaping ((Progress) -> Void),
+                                   completion: @escaping RequestCompletionMultipleClosure){
+        self.requestArrayWithProgress(progressCallback, completion: completion)
+    }
+    /**
+     Calls the request for the specified Endpoint
+     - parameters:
+        - progressCallback: Callback that returns the progress of the current request.
+        - completion: Callback for the response of the request.
+     */
+    private func requestArrayWithProgress(_ progressCallback:((Progress) -> Void)? = nil,
+                 completion:RequestCompletionMultipleClosure? = nil)
+    {
+        //Check for headers available for the route
+        //If the current request is a guest,
+        // the header is not null
+        guard let headers = headers else {
+            if let completion = completion{
+                completion([], Helpers.makeError(with: "Unauthorized access. Token may have expired."))
+                //must implement a force logout functionality here
+            }
+            return
+        }
+        
+        //The developer can choose to log the request
+        // or not.
+        if shouldLog && shouldLogRequest{
+            print("Request URL: \(url.absoluteString)")
+            if headers.count > 0{
+                print("Header: \(headers.dictionary.toJSONString())")
+            }
+            print("Method: \(method.rawValue)")
+            if let parameters = parameters,
+               parameters.count > 0{
+                print("Parameters: \(parameters.toJSONString())")
+            }
+        }
+        
+        //Starts executing the request in hear
+        AF.request(url,
+                method: method,
+                parameters: parameters,
+                encoding: parameterEncoding,
+                headers: headers).downloadProgress(closure: { (progress) in
+                //If the developer provided a callback for progress,
+                // the callback will be called through here
+                if let progressCallback = progressCallback{
+                    print("progress: \(progress.fractionCompleted)")
+                    DispatchQueue.main.async {
+                        progressCallback(progress)
+                    }
+                }
+        }).responseJSON(completionHandler: { (response) in
+            self.handleResponse(parameters: parameters,
+                                    response: response,
+                                    progressCallback: progressCallback,
+                                    completion: completion)
+        })
+    }
+    
+    fileprivate func handleResponse(parameters: Parameters? = nil,
+                                        response: DataResponse<Any, AFError>,
+                                        progressCallback:((Progress) -> Void)? = nil,
+                                        completion:RequestCompletionMultipleClosure? = nil)
+    {
+        //The developer can choose to log the result specifically.
+        // If the logging of request was disabled by default,
+        // The result will not be logged either.
+        if shouldLog && shouldLogResponse{
+            print("Response for URL: \(url.absoluteString)")
+            do{
+                let raw = try response.result.get()
+                let json = JSON(raw)
+                if let rawString = json.rawString(){
+                    print(rawString)
+                }else{
+                    print(json)
+                }
+            }catch{
+                print(error)
+            }
+        }
+        
+        DispatchQueue.main.async {
+            //error is not being thrown if the token is not expired from the backend
+            //so better handle it in this block
+            if let statusCode = response.response?.statusCode{
+                print("Status \(statusCode)")
+                if statusCode == 404{
+                    // handle 404
+                    return
+                }
+                else if statusCode == 401{
+                    // handle 401
+                    return
+                }
+                else if statusCode == 403{
+                    // handle 403
+                }
+                else if statusCode == 500{
+                    // handle 500
+                }
+            }
+            
+            switch response.result{
+            case .success(let json):
+                self.serializeResponse(with: json, completion: completion)
+                break
+            case .failure(let error):
+                print("An error occured while attempting to process the request")
+                print(error)
+                if error.localizedDescription.lowercased().contains("offline"){
+                    completion?([], Helpers.makeOfflineError())
+                }else{
+                    completion?([], error)
+                }
+            }
+        }
+    }
+    
+    func serializeResponse(with object: Any, completion:RequestCompletionMultipleClosure? = nil){
+        print("Serialized regular")
+        completion?([object as? ResultingModel].compactMap({$0}), nil)
+    }
+}
+
+public extension PRequest where ResultingModel == JSON{
+    func serializeResponse(with object: Any, completion: RequestCompletionMultipleClosure? = nil){
+        print("Serialized JSON")
+        completion?([ResultingModel(object)], nil)
+    }
+}
+
+public extension PRequest where ResultingModel: Any & Mappable{
+    func serializeResponse(with object: Any, completion: RequestCompletionMultipleClosure? = nil){
+        print("Serialized mappable")
+        var json: JSON? = JSON(object)
+        
+        dictionarySearchNestedKeys.forEach { (key) in
+            json = json?.dictionary?[key]
+        }
+        
+        if let dictionaryObject = json?.dictionaryObject{
+            guard let object = ResultingModel(JSON: dictionaryObject) else{
+                completion?([], Helpers.makeError(with: "Could not parse object to \(ResultingModel.self)"))
+                return
+            }
+            completion?([object], nil)
+        }else if let objectArray = json?.array{
+            let objects = objectArray.compactMap({ json -> ResultingModel? in
+                guard let rawObject = json.dictionaryObject else{
+                    return nil
+                }
+                return ResultingModel(JSON: rawObject)
+            })
+            completion?(objects, nil)
+        }else{
+            completion?([], Helpers.makeError(with: "Could not parse JSON!"))
+        }
+    }
+}
+
--- Sources/PatWeb/PRequest.swift
+++ Sources/PatWeb/PRequest.swift
@@ -12,7 +12,8 @@ import ObjectMapper
 public typealias OptionalParameters = [String: Any?]
 
 public protocol PRequest{
-    typealias RequestCompletionClosure = ([ResultingModel], Error?) -> Void
+    typealias RequestCompletionSingleClosure = (ResultingModel?, Error?) -> Void
+    typealias RequestCompletionMultipleClosure = ([ResultingModel], Error?) -> Void
     
     associatedtype ResultingModel = JSON
     
@@ -29,7 +30,7 @@ public protocol PRequest{
     
     // MARK: Request Handlers
     var dictionarySearchNestedKeys: [String] { get }
-    func serializeResponse(with object: Any, completion:RequestCompletionClosure?)
+    func serializeResponse(with object: Any, completion:RequestCompletionMultipleClosure?)
     
     // MARK: Request Logging
     var shouldLog: Bool { get }
@@ -83,166 +84,6 @@ public extension PRequest{
             }else{
                 return URLEncoding.default
             }
-        }
-    }
-    
-    /**
-     Calls the request for the specified Endpoint
-     - parameters:
-        - parameters: The parameters for the request.
-        - progressCallback: Callback that returns the progress of the current request.
-        - completion: Callback for the response of the request.
-        - shouldLog: Specified to allow the current requet log before request and response.
-        - shouldLogResult: If request is allowed to log, user can choose to not log the result.
-     */
-    func request(progressCallback:((Progress) -> Void)? = nil,
-                 completion:RequestCompletionClosure? = nil)
-    {
-        //Check for headers available for the route
-        //If the current request is a guest,
-        // the header is not null
-        guard let headers = headers else {
-            if let completion = completion{
-                completion([], Helpers.makeError(with: "Unauthorized access. Token may have expired."))
-                //must implement a force logout functionality here
-            }
-            return
-        }
-        
-        //The developer can choose to log the request
-        // or not.
-        if shouldLog && shouldLogRequest{
-            print("Request URL: \(url.absoluteString)")
-            if headers.count > 0{
-                print("Header: \(headers.dictionary.toJSONString())")
-            }
-            print("Method: \(method.rawValue)")
-            if let parameters = parameters,
-               parameters.count > 0{
-                print("Parameters: \(parameters.toJSONString())")
-            }
-        }
-        
-        //Starts executing the request in hear
-        AF.request(url,
-                method: method,
-                parameters: parameters,
-                encoding: parameterEncoding,
-                headers: headers).downloadProgress(closure: { (progress) in
-                //If the developer provided a callback for progress,
-                // the callback will be called through here
-                if let progressCallback = progressCallback{
-                    print("progress: \(progress.fractionCompleted)")
-                    DispatchQueue.main.async {
-                        progressCallback(progress)
-                    }
-                }
-        }).responseJSON(completionHandler: { (response) in
-            self.handleResponse(parameters: parameters,
-                                    response: response,
-                                    progressCallback: progressCallback,
-                                    completion: completion)
-        })
-    }
-    
-    fileprivate func handleResponse(parameters: Parameters? = nil,
-                                        response: DataResponse<Any, AFError>,
-                                        progressCallback:((Progress) -> Void)? = nil,
-                                        completion:RequestCompletionClosure? = nil)
-    {
-        //The developer can choose to log the result specifically.
-        // If the logging of request was disabled by default,
-        // The result will not be logged either.
-        if shouldLog && shouldLogResponse{
-            print("Response for URL: \(url.absoluteString)")
-            do{
-                let raw = try response.result.get()
-                let json = JSON(raw)
-                if let rawString = json.rawString(){
-                    print(rawString)
-                }else{
-                    print(json)
-                }
-            }catch{
-                print(error)
-            }
-        }
-        
-        DispatchQueue.main.async {
-            //error is not being thrown if the token is not expired from the backend
-            //so better handle it in this block
-            if let statusCode = response.response?.statusCode{
-                print("Status \(statusCode)")
-                if statusCode == 404{
-                    // handle 404
-                    return
-                }
-                else if statusCode == 401{
-                    // handle 401
-                    return
-                }
-                else if statusCode == 403{
-                    // handle 403
-                }
-                else if statusCode == 500{
-                    // handle 500
-                }
-            }
-            
-            switch response.result{
-            case .success(let json):
-                self.serializeResponse(with: json, completion: completion)
-                break
-            case .failure(let error):
-                print("An error occured while attempting to process the request")
-                print(error)
-                if error.localizedDescription.lowercased().contains("offline"){
-                    completion?([], Helpers.makeOfflineError())
-                }else{
-                    completion?([], error)
-                }
-            }
-        }
-    }
-    
-    func serializeResponse(with object: Any, completion:RequestCompletionClosure? = nil){
-        print("Serialized regular")
-        completion?([object as? ResultingModel].compactMap({$0}), nil)
-    }
-}
-
-public extension PRequest where ResultingModel == JSON{
-    func serializeResponse(with object: Any, completion: RequestCompletionClosure? = nil){
-        print("Serialized JSON")
-        completion?([ResultingModel(object)], nil)
-    }
-}
-
-public extension PRequest where ResultingModel: Any & Mappable{
-    func serializeResponse(with object: Any, completion: RequestCompletionClosure? = nil){
-        print("Serialized mappable")
-        var json: JSON? = JSON(object)
-        
-        dictionarySearchNestedKeys.forEach { (key) in
-            json = json?.dictionary?[key]
-        }
-        
-        if let dictionaryObject = json?.dictionaryObject{
-            guard let object = ResultingModel(JSON: dictionaryObject) else{
-                completion?([], Helpers.makeError(with: "Could not parse object to \(ResultingModel.self)"))
-                return
-            }
-            completion?([object], nil)
-        }else if let objectArray = json?.array{
-            let objects = objectArray.compactMap({ json -> ResultingModel? in
-                guard let rawObject = json.dictionaryObject else{
-                    return nil
-                }
-                return ResultingModel(JSON: rawObject)
-            })
-            completion?(objects, nil)
-        }else{
-            completion?([], Helpers.makeError(with: "Could not parse JSON!"))
         }
     }
 }
